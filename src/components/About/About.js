c10b4880b969aec0ea4d5d1271bbdd08ad014e69
David <david94hill@gmail.com>
2022-07-28 10:27:32 +0100

    init

---
+++ src/components/About/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { 
+  AboutContainer,
+  AboutHeader
+ } from './AboutElements';
+
+const About = () => {
+  return (
+    <AboutContainer>
+      <AboutHeader>About</AboutHeader>
+    </AboutContainer>
+  )
+}
+
+export default About
---
+++ src/components/About/AboutElements.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const AboutContainer = styled.div`
+height: 100vh;
+width: 100%;
+display: flex;
+align-items: center;
+justify-content: center;
+font-family: 'Roboto Mono', monospace;
+`
+
+export const AboutHeader = styled.h1`
+font-size: 3rem;
+color: #5ca386;
+`
